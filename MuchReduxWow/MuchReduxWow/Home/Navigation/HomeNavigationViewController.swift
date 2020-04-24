c6be5bd6310d0f4578b68a63cfef0e80e7caf0a8
Pawel Kowalczuk <pwl.kowalczuk@gmail.com>
2020-04-24 06:21:20 +0200

    old project removed

--- MuchReduxWow/MuchReduxWow/Home/Navigation/HomeNavigationViewController.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-class HomeNavigationViewController: UINavigationController, Coordinated {
-    weak var coordinator: Coordinator?
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .white   
-    }
-}
--- MuchReduxWow/MuchReduxWow/Profile/Navigation/ProfileNavigationController.swift
+++
@@ -1,5 +0,0 @@
-import UIKit
-
-class ProfileNavigationController: UINavigationController, Coordinated {
-    weak var coordinator: Coordinator?
-}
