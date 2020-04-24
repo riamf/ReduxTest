60bdf5bacb2c458eba9be3d0f62321b3aaff7b32
Pawel Kowalczuk <pwl.kowalczuk@gmail.com>
2020-04-24 06:01:35 +0200

    old project deleted

--- MuchReduxWow/MuchReduxWow/Redux/Environment.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-class AppEnvironment {
-    let store = ReduxStore(state: nil,
-                           reducer: AppState.appStateReducer,
-                           middleware: [M.AppState, M.SearchResults, M.Filters])
-    let useCaseFactory = UseCaseFactory()
-    var coordinator: Coordinator!
-}
